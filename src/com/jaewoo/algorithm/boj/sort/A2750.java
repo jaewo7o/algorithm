719e00d1b3ec725e6585e335b8a201621d3d885d
jaewoo <jaewo7o@naver.com>
2021-02-14 00:23:12 +0900

    bubble sort basic coding

--- src/com/jaewoo/algorithm/boj/sort/A2750.java
+++ src/com/jaewoo/algorithm/boj/sort/A2750.java
@@ -8,9 +8,11 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class A2750 {
+    static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
         int[] numbers = new int[N];
         for (int i=0; i<N; i++) {
@@ -18,18 +20,49 @@ public class A2750 {
         }
 
         int[] result1 = selectionSort(Arrays.copyOf(numbers, N));
+        System.out.println("selectionSort Result : \n");
         IntStream.of(result1).forEach(System.out::println);
+
+        int[] result2 = bubbleSort(Arrays.copyOf(numbers, N));
+        System.out.println("bubbleSort Result : \n");
+        IntStream.of(result2).forEach(System.out::println);
+
+        int[] result3 = insertionSort(Arrays.copyOf(numbers, N));
+        System.out.println("insertionSort Result\n");
+        IntStream.of(result3).forEach(System.out::println);
+    }
+
+    private static int[] insertionSort(int[] numbers) {
+
+        return numbers;
+    }
+
+    private static int[] bubbleSort(int[] numbers) {
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N-1-i; j++) {
+                if (numbers[j] > numbers[j+1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                }
+            }
+        }
+        return numbers;
     }
 
     public static int[] selectionSort(int[] numbers) {
-        for (int i=0; i<numbers.length-1; i++) {
-            for (int j=i+1; j<numbers.length; j++) {
-                if (numbers[i] > numbers[j]) {
-                    int temp = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = temp;
+        for (int i=0; i<N-1; i++) {
+            int min = numbers[i];
+            int minIndex = i;
+            for (int j=i+1; j<N; j++) {
+                if (min > numbers[j]) {
+                    min = numbers[j];
+                    minIndex = j;
                 }
             }
+
+            numbers[minIndex] = numbers[i];
+            numbers[i] = min;
         }
 
         return numbers;
