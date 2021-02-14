c5a6d5cf0675bf526bbd70bbb1f097e21e9d2b30
jaewoo <jaewo7o@naver.com>
2021-02-14 18:17:47 +0900

    insertions sort

--- src/com/jaewoo/algorithm/boj/sort/A2750.java
+++ src/com/jaewoo/algorithm/boj/sort/A2750.java
@@ -15,35 +15,47 @@ public class A2750 {
         N = Integer.parseInt(br.readLine());
 
         int[] numbers = new int[N];
-        for (int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             numbers[i] = Integer.parseInt(br.readLine());
         }
 
         int[] result1 = selectionSort(Arrays.copyOf(numbers, N));
-        System.out.println("selectionSort Result : \n");
-        IntStream.of(result1).forEach(System.out::println);
+        System.out.print("selectionSort Result : ");
+        IntStream.of(result1).forEach(x -> System.out.print(x + " "));
+        System.out.println("\n");
 
         int[] result2 = bubbleSort(Arrays.copyOf(numbers, N));
-        System.out.println("bubbleSort Result : \n");
-        IntStream.of(result2).forEach(System.out::println);
+        System.out.print("bubbleSort Result : ");
+        IntStream.of(result2).forEach(x -> System.out.print(x + " "));
+        System.out.println("\n");
 
         int[] result3 = insertionSort(Arrays.copyOf(numbers, N));
-        System.out.println("insertionSort Result\n");
-        IntStream.of(result3).forEach(System.out::println);
+        System.out.print("insertionSort Result : ");
+        IntStream.of(result3).forEach(x -> System.out.print(x + " "));
     }
 
     private static int[] insertionSort(int[] numbers) {
-
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0; j--) {
+                if (numbers[j - 1] > numbers[j]) {
+                    swap(numbers, j - 1, j);
+                }
+            }
+        }
         return numbers;
     }
 
+    private static void swap(int[] numbers, int a, int b) {
+        int temp = numbers[a];
+        numbers[a] = numbers[b];
+        numbers[b] = temp;
+    }
+
     private static int[] bubbleSort(int[] numbers) {
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<N-1-i; j++) {
-                if (numbers[j] > numbers[j+1]) {
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    swap(numbers, j, j + 1);
                 }
             }
         }
@@ -51,10 +63,10 @@ public class A2750 {
     }
 
     public static int[] selectionSort(int[] numbers) {
-        for (int i=0; i<N-1; i++) {
+        for (int i = 0; i < N - 1; i++) {
             int min = numbers[i];
             int minIndex = i;
-            for (int j=i+1; j<N; j++) {
+            for (int j = i + 1; j < N; j++) {
                 if (min > numbers[j]) {
                     min = numbers[j];
                     minIndex = j;
@@ -76,4 +88,4 @@ public class A2750 {
 3
 4
 1
- */
+*/
