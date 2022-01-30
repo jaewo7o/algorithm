72ec4d29534b03f2cdfadb5f123c143dc05be382
Jung Jaewoo <jaewo7o@naver.com>
2022-01-31 01:51:54 +0900

    이분매칭 알고리즘 추가

--- src/com/jaewoo/algorithm/boj/graph/bipartite_graph/A1017.java
+++ src/com/jaewoo/algorithm/boj/graph/bipartite_graph/A1017.java
@@ -3,20 +3,19 @@ package com.jaewoo.algorithm.boj.graph.bipartite_graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class A1017 {
 
-    private static int MAX_NUMBER = 50;
-    private static int [] numbers = new int[MAX_NUMBER + 1];
-    private static int [] numbersA = new int[MAX_NUMBER + 1];
-    private static int [] numbersB = new int[MAX_NUMBER + 1];
+    private static int MAX_NUMBER = 1000;
+    private static int MAX_NUMBER_COUNT = 50;
+    private static int [] numbers = new int[MAX_NUMBER_COUNT + 1];
+    private static int [] numbersA = new int[MAX_NUMBER_COUNT + 1];
+    private static int [] numbersB = new int[MAX_NUMBER_COUNT + 1];
 
     private static List<Integer>[] links;
     private static int[] d;
+    private static boolean[] isVisit;
 
     private static int setACount = 0;
     private static int setBCount = 0;
@@ -24,6 +23,7 @@ public class A1017 {
     private static int[] primeSieve = new int[MAX_NUMBER * 2 + 1];
 
     public static void main(String[] args) throws IOException {
+        // 아리스토테네스의 체 생성(소수 비교 목적)
         initPrimeSieve();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,6 +37,7 @@ public class A1017 {
             links[i] = new ArrayList<>();
         }
 
+        // 홀수/짝수에 따라 그룹A, 그룹B로 분리
         for (int i = 1; i <= n; i++) {
             if (numbers[1] % 2 == numbers[i] % 2) {
                 numbersA[++setACount] = numbers[i];
@@ -45,6 +46,7 @@ public class A1017 {
             }
         }
 
+        // 그룹간의 합이 소수인 경우 두 그룹 간의 관계 연결
         for (int i = 1; i <= setACount; i++) {
             for (int j = 1; j <= setBCount; j++) {
                 if (primeSieve[numbersA[i] + numbersB[j]] != 0) {
@@ -53,20 +55,25 @@ public class A1017 {
             }
         }
 
+        d = new int[setBCount + 1];
+        isVisit = new boolean[setBCount + 1];
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int j = 1; j <= setBCount; j++) {
-            if (primeSieve[numbersA[1] + numbersB[j]] != 0) {
-                d = new int[setBCount + 1];
 
+        // 그룹A의 첫번째 요소를 고정하고 그룹B의 대상을 바꿔가며 전체 매칭되는 케이스 체크
+        for (int j = 1; j <= setBCount; j++) {
+            if (links[1].contains(j)) {
+                Arrays.fill(d, -1);
                 d[j] = 1;
                 int matchingCount = 1;
 
                 for (int i = 2; i <= setACount; i++) {
-                    if (dfs(i)) {
+                    Arrays.fill(isVisit, false);
+                    if (dfs(i, j)) {
                         matchingCount++;
                     }
                 }
 
+                // 매칭 건수가 제시한 숫자의 건수의 반이 된다면 전체 매칭 됨
                 if (matchingCount == n /2 ) {
                     pq.offer(numbersB[j]);
                 }
@@ -85,9 +92,17 @@ public class A1017 {
         }
     }
 
-    private static boolean dfs(int x) {
+    private static boolean dfs(int x, int initY) {
         for (int y : links[x]) {
-            if (d[y] == 0 || dfs(d[y])) {
+            // 한번 방문한 그룹B의 요소는 다시 방문할 필요가 없음
+            // 초기 그룹B의 요소가 나오는 경우 매칭값을 변경하면 안되기 때문에 skip
+            if (isVisit[y] || y == initY) {
+                continue;
+            }
+
+            isVisit[y] = true;
+
+            if (d[y] == -1 || dfs(d[y], initY)) {
                 d[y] = x;
                 return true;
             }
