de88e008d5d6057369a1b958dd48c5b6d5b1db8d
Jung Jaewoo <jaewo7o@naver.com>
2022-01-30 17:40:30 +0900

    네트워크플로우 예제문제 추가

--- src/com/jaewoo/algorithm/boj/graph/maximum_flow/A2316.java
+++ src/com/jaewoo/algorithm/boj/graph/maximum_flow/A2316.java
@@ -5,13 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class A17412 {
+public class A2316 {
+    /*
+        백준문제 URL : https://www.acmicpc.net/problem/2316
+     */
 
     private static int N;
     private static int P;
     private static List<Integer>[] links;
     private static int[][] flow;
     private static int[] prev;
+    private static boolean[] isVisit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,6 +27,7 @@ public class A17412 {
         links = new List[N + 1];
         flow = new int[N + 1][N + 1];
         prev = new int[N + 1];
+        isVisit = new boolean[N + 1];
         for (int i = 1; i <= N; i++) {
             links[i] = new ArrayList<>();
         }
@@ -35,6 +40,7 @@ public class A17412 {
             e = Integer.parseInt(token.nextToken());
 
             links[s].add(e);
+            links[e].add(s);
         }
 
         int maxFlow = maxFlow(1, 2);
@@ -44,23 +50,19 @@ public class A17412 {
 
     private static int maxFlow(int start, int end) {
         int result = 0;
-
         while (true) {
             Arrays.fill(prev, -1);
 
-            Queue<Integer> q = new LinkedList<>();
+            Queue<Integer> q = new LinkedList();
             q.offer(start);
 
             while (!q.isEmpty()) {
                 int current = q.poll();
                 for (int next : links[current]) {
-                    if (flow[current][next] < 1 && prev[next] == -1) {
+                    if (flow[current][next] < 1 && prev[next] == -1 && !isVisit[next]) {
                         q.offer(next);
-
-                        // 경로를 기록
                         prev[next] = current;
 
-                        // 도착점에 도달하면 종료처리
                         if (next == end) {
                             break;
                         }
@@ -68,21 +70,23 @@ public class A17412 {
                 }
             }
 
-            // 더이상 도착점에 도달하는 경우가 없으면 종료처리
             if (prev[end] == -1) {
                 break;
             }
 
-            // 거꾸로 최소유량을 탐색한다.
             int tempFlow = Integer.MAX_VALUE;
-            for (int i = end; i != start; i = prev[i]) {
+            for (int i = end; i != start; i = prev[i] ) {
                 tempFlow = Math.min(tempFlow, 1 - flow[prev[i]][i]);
             }
 
-            // 최소유량 만큼 추가
-            for (int i = end; i != start; i = prev[i]) {
+            for (int i = end; i != start; i = prev[i] ) {
                 flow[prev[i]][i] += tempFlow;
                 flow[i][prev[i]] -= tempFlow;
+
+                // start & end는 중복방문 가능하도록 방문처리 없음
+                if (i != end) {
+                    isVisit[i] = true;
+                }
             }
 
             result += tempFlow;
@@ -91,7 +95,6 @@ public class A17412 {
         return result;
     }
 }
-
 
 /* INPUT
 5 5
