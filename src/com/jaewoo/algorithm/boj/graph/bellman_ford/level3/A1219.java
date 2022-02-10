280e521025056c0eead43179a157f25948da2ede
jaewo7o <jaewo7o@naver.com>
2022-02-10 11:15:46 +0900

    백준 1219 로직 정비

--- src/com/jaewoo/algorithm/boj/graph/bellman_ford/level3/A1219.java
+++ src/com/jaewoo/algorithm/boj/graph/bellman_ford/level3/A1219.java
@@ -31,7 +31,7 @@ public class A1219 {
 
         edges = new Edge[M];
 
-        for (int i=0, u, v, w; i<M; i++) {
+        for (int i = 0, u, v, w; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
             u = Integer.parseInt(st.nextToken());
@@ -43,60 +43,59 @@ public class A1219 {
         }
 
         st = new StringTokenizer(br.readLine());
-        for (int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             earns[i] = Integer.parseInt(st.nextToken());
         }
 
-        bellmanFord();
-        printResult();
-    }
+        boolean isCycle = bellmanFord();
 
-    private static void bellmanFord() {
-        Arrays.fill(dist, Integer.MIN_VALUE);
-
-        dist[0] = earns[0];
-        for (int i=0; i<N+100; i++) {
-            for (int j=0; j<M; j++) {
-                Edge edge = edges[j];
-
-                int s = edge.start;
-                int e = edge.end;
-
-                // Relaxation 불필요
-                if (dist[s] == Integer.MIN_VALUE) {
-                    continue;
-                } else if (dist[s] == Integer.MAX_VALUE) {
-                    dist[e] = Integer.MAX_VALUE;
-                } else if (dist[e] < dist[s] + edge.cost + earns[e]) {
-                    dist[e] = dist[s] + edge.cost + earns[e];
-
-                    if (i >= N-1) {
-                        dist[e] = Integer.MAX_VALUE;
-                    }
-                }
-            }
-        }
-    }
-
-    private static void printResult() {
-        if (dist[E] == -Integer.MIN_VALUE) {
+        if (dist[E] == Integer.MIN_VALUE) {
             System.out.println("gg");
-        } else if (dist[E] == Integer.MAX_VALUE) {
+        } else if (isCycle) {
             System.out.println("Gee");
         } else {
             System.out.println(dist[E]);
         }
     }
 
+    private static boolean bellmanFord() {
+        Arrays.fill(dist, Integer.MIN_VALUE);
+
+        dist[0] = earns[0];
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j < M; j++) {
+                Edge edge = edges[j];
+
+                int s = edge.s;
+                int e = edge.e;
+
+                // Relaxation 불필요
+                if (dist[s] == Integer.MIN_VALUE) {
+                    continue;
+                } else if (dist[s] == Integer.MAX_VALUE) {
+                    dist[e] = Integer.MAX_VALUE;
+                } else if (dist[e] < dist[s] + edge.c + earns[e]) {
+                    dist[e] = dist[s] + edge.c + earns[e];
+
+                    if (i == N) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
     public static class Edge {
-        public int start;
-        public int end;
-        public int cost;
+        public int s;
+        public int e;
+        public int c;
 
         public Edge(int start, int end, int cost) {
-            this.start = start;
-            this.end = end;
-            this.cost = cost;
+            this.s = start;
+            this.e = end;
+            this.c = cost;
         }
     }
 }
