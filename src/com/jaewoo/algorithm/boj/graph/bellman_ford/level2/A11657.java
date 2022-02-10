0bb8c02ab14851159375314ade3bda4d06097b8d
jaewo7o <jaewo7o@naver.com>
2022-02-10 10:33:17 +0900

    백준 11657 알고리즘 정비

--- src/com/jaewoo/algorithm/boj/graph/bellman_ford/level2/A11657.java
+++ src/com/jaewoo/algorithm/boj/graph/bellman_ford/level2/A11657.java
@@ -14,8 +14,6 @@ public class A11657 {
     static int M;
 
     static Edge[] links;
-    static List<Edge>[] linkEdges;
-
     static int[] dist;
 
     public static void main(String[] args) throws IOException {
@@ -26,13 +24,7 @@ public class A11657 {
         M = Integer.parseInt(st.nextToken());
 
         dist = new int[N + 1];
-
         links = new Edge[M + 1];
-//        linkEdges = new List[N + 1];
-//
-//        for (int i=1; i<=N; i++) {
-//            linkEdges[i] = new ArrayList<>();
-//        }
 
         for (int i = 1, u, v, w; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -40,20 +32,11 @@ public class A11657 {
             v = Integer.parseInt(st.nextToken());
             w = Integer.parseInt(st.nextToken());
 
-            Edge e = new Edge(u, v, w);
-
-            links[i] = e;
-
-//            linkEdges[u].add(e);
+            links[i] = new Edge(u, v, w);
         }
 
-        System.out.println("Bellman Ford Output!");
         boolean cycle = bellmanFord(1);
         printOutput(cycle);
-
-        //System.out.println("SPFA Output!");
-        //cycle = spfa(1);
-        //printOutput(cycle);
     }
 
     private static void printOutput(boolean cycle) {
@@ -85,39 +68,6 @@ public class A11657 {
                     if (i == N) {
                         return true;
                     }
-                }
-            }
-        }
-
-        return false;
-    }
-
-    public static boolean spfa(int start) {
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
-
-        int[] cycle = new int[N + 1];
-
-        Queue<Integer> q = new LinkedList();
-        q.offer(start);
-
-        cycle[start]++;
-
-        while (!q.isEmpty()) {
-            int now = q.poll();
-
-            for (Edge edge : linkEdges[now]) {
-                int nextCost = dist[now] + edge.w;
-                int end = edge.v;
-                if (dist[end] > nextCost) {
-                    dist[end] = nextCost;
-
-                    cycle[end]++;
-                    if (cycle[end] >= N) {
-                        return true;
-                    }
-
-                    q.offer(edge.v);
                 }
             }
         }
