95da3747a3ba785b2f580439cf5936413fcc54de
jaewo7o <jaewo7o@naver.com>
2022-02-15 17:59:41 +0900

    10217 알고리즘 정비

--- src/com/jaewoo/algorithm/boj/graph/dijkstra/level3/A10217.java
+++ src/com/jaewoo/algorithm/boj/graph/dijkstra/level3/A10217.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class A10217 {
+    private static int N;
+    private static int M;
 
     private static List<Ticket>[] tickets;
-    private static int[] times;
+    private static int[][] times;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,11 +20,11 @@ public class A10217 {
             String line = br.readLine();
             StringTokenizer token = new StringTokenizer(line);
 
-            int N = Integer.parseInt(token.nextToken());
-            int M = Integer.parseInt(token.nextToken());
+            N = Integer.parseInt(token.nextToken());
+            M = Integer.parseInt(token.nextToken());
             int K = Integer.parseInt(token.nextToken());
 
-            times = new int[N + 1];
+            times = new int[N + 1][M + 1];
             tickets = new List[N + 1];
             for (int n = 1; n <= N; n++) {
                 tickets[n] = new ArrayList<>();
@@ -39,61 +41,84 @@ public class A10217 {
                 tickets[u].add(new Ticket(u, v, c, d));
             }
 
-            boolean isReachable = dijkstra(1, M);
+            dijkstra(1);
 
-            if (!isReachable) {
+            int minTime = Integer.MAX_VALUE;
+            for (int i = 1; i <= M; i++) {
+                minTime = Math.min(minTime, times[N][i]);
+            }
+
+            if (minTime == Integer.MAX_VALUE) {
                 System.out.println("Poor KCM");
             } else {
-                System.out.println(times[N]);
+                System.out.println(minTime);
             }
         }
     }
 
-    private static boolean dijkstra(int start, int M) {
-        Arrays.fill(times, Integer.MAX_VALUE);
+    private static void dijkstra(int start) {
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(times[i], Integer.MAX_VALUE);
+        }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.offer(start);
-        times[start] = 0;
+        PriorityQueue<Airport> pq = new PriorityQueue<>();
+        pq.offer(new Airport(start, 0, 0));
+        times[start][0] = 0;
 
-        int nextTimes = 0;
         while (!pq.isEmpty()) {
-            int current = pq.poll();
+            Airport airport = pq.poll();
 
-            for (Ticket t : tickets[current]) {
-                nextTimes = times[current] + t.d;
-                if (times[t.v] > nextTimes) {
-                    times[t.v] = nextTimes;
-                    M = M - t.c;
+            // 마지막 공항에 도착하면 종료
+            if (airport.node == N) {
+                break;
+            }
 
-                    if (M < 0) {
-                        return false;
-                    }
+            for (Ticket t : tickets[airport.node]) {
+                int nextNode = t.end;
+                int nextTimes = airport.time + t.time;
+                int nextMoney = airport.money + t.money;
 
-                    pq.offer(t.v);
+                // 돈을 초과하는 경우 이동 불가
+                if (nextMoney > M) {
+                    continue;
+                }
+
+                if (times[nextNode][nextMoney] > nextTimes) {
+                    times[nextNode][nextMoney] = nextTimes;
+                    pq.offer(new Airport(nextNode, nextMoney, nextTimes));
                 }
             }
         }
-
-        return true;
     }
 
-    public static class Ticket implements Comparable<Ticket> {
-        public int u;
-        public int v;
-        public int c;
-        public int d;
+    public static class Ticket {
+        public int start;
+        public int end;
+        public int money;
+        public int time;
 
-        public Ticket(int u, int v, int c, int d) {
-            this.u = u;
-            this.v = v;
-            this.c = c;
-            this.d = d;
+        public Ticket(int start, int end, int money, int time) {
+            this.start = start;
+            this.end = end;
+            this.money = money;
+            this.time = time;
+        }
+    }
+
+    private static class Airport implements Comparable<Airport> {
+        public int node;
+        public int money;
+        public int time;
+
+        public Airport(int node, int money, int time) {
+            this.node = node;
+            this.money = money;
+            this.time = time;
         }
 
         @Override
-        public int compareTo(Ticket t) {
-            return this.d - t.d;
+        public int compareTo(Airport a) {
+            return this.time - a.time;
         }
     }
 }
