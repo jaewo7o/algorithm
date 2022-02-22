5cc59bfa46c68e4a86385ce00d0a5d61aeb89ec4
Jung Jaewoo <jaewo7o@naver.com>
2022-02-23 06:36:17 +0900

    indextree sample 추가

--- src/com/jaewoo/algorithm/boj/tree/indextree/level2/A2042.java
+++ src/com/jaewoo/algorithm/boj/tree/indextree/level2/A2042.java
@@ -25,11 +25,12 @@ public class A2042 {
             st = new StringTokenizer(br.readLine().trim());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
             if (a == 1) {
                 tree.update(b, c);
-            } else
-                System.out.println(tree.getSum(b, c));
+            } else {
+                System.out.println(tree.getSum(b, (int) c));
+            }
         }
     }
 
@@ -59,50 +60,60 @@ public class A2042 {
         private long makeSubTree(int node, int left, int right) {
             if (left == right) {
                 if (left < numbers.length) {
+                    // leaf node에 입력 숫자를 초기화 해준다.
                     return this.nodes[node] = this.numbers[left];
                 } else {
+                    // leaf node 중에 입력 숫자 범위 밖은 0으로 초기화 해준다.
                     return this.nodes[node] = 0;
                 }
             }
 
+            // 중간합 값을 인덱스트리 노드에 업데이트 한다.
             int mid = (left + right) / 2;
-            this.nodes[node] = makeSubTree(2*node, left, mid);
-            this.nodes[node] += makeSubTree(2*node + 1, mid + 1, right);
+            this.nodes[node] = makeSubTree(2 * node, left, mid);
+            this.nodes[node] += makeSubTree(2 * node + 1, mid + 1, right);
             return this.nodes[node];
         }
 
-        public long getSum(int left, int right) {
-            return getSubSum(1, 1, leafCount, left, right);
+        public long getSum(int tLeft, int tRight) {
+            return getSubSum(1, 1, leafCount, tLeft, tRight);
         }
 
         private long getSubSum(int node, int left, int right, int tLeft, int tRight) {
+            // 부분합 대상 범위 밖이면 0
             if (tRight < left || tLeft > right) {
-                return 0L;
+                return 0;
             }
 
-            if (tLeft <= left  && right <= tRight) {
+            // 부분합 대상 범위내로 들어오면 인덱스 노드 값을 반환
+            if (tLeft <= left && right <= tRight) {
                 return this.nodes[node];
             }
 
             int mid = (left + right) / 2;
             return getSubSum(node * 2, left, mid, tLeft, tRight)
-                    + getSubSum(node*2 + 1, mid + 1, right, tLeft, tRight );
+                    + getSubSum(node * 2 + 1, mid + 1, right, tLeft, tRight);
         }
 
-        public void update(int index, int value) {
+        public void update(int index, long value) {
             subUpdate(1, 1, leafCount, index, value - numbers[index]);
             this.numbers[index] = value;
         }
 
         private void subUpdate(int node, int left, int right, int index, long diff) {
-            if (index >= left && index <= right) {
+            // 업데이트 index가 노드 범위 밖이면 업데이트 대상이 아니기 때문에 종료 처리
+            if (left > index || index > right) {
+                return;
+            }
+
+            // 업데이트 index가 노드 범위 내에 들어오면 입력 차이만큼 업데이트
+            if (left <= index && index <= right) {
                 this.nodes[node] += diff;
 
+                // 말단 노드이면 재귀함수 종료처리
                 if (left == right) {
                     return;
                 }
-            } else {
-                return;
             }
 
             int mid = (left + right) / 2;
